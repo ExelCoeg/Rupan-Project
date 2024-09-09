032b0ab2534429246c00ddba245010c9af20a1d3
Exel Boy Alfanso <72937148+ExelCoeg@users.noreply.github.com>
2024-09-09 17:25:09 +0700

    created outline

---
+++ Assets/Scripts/InteractableObject/InteractableObject.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+public abstract class InteractableObject : MonoBehaviour
+{
+    public bool isDebug = false;
+    Material outline;
+    MeshRenderer meshRenderer;
+    public bool is2DObject;
+    
+    public abstract void Interacted(); 
+    public void Awake() {
+        if(TryGetComponent<MeshRenderer>(out MeshRenderer meshRenderer)){
+            this.meshRenderer = meshRenderer;
+            outline = meshRenderer.materials[1];
+        }
+        
+       
+    }
+    private void Start() {
+        DisableOutline();        
+    }
+   public void DisableOutline(){
+        outline.SetFloat("_Scale", 0f);
+    }
+    public void EnableOutline(){
+        outline.SetFloat("_Scale", 1.125f);
+   }
+ 
+
+   
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,25 +2,72 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
-    Vector3 move;
     Rigidbody rb;
-
     public float speed;
-    public InputAction playerControls;   
+    public PlayerInputActions playerControls;   
+    public InputAction move;
+    public InputAction fire;
+    public InputAction interact;
+    public InteractableObject currentInteractableObject;
+    Vector3 moveDirection;
+    RaycastHit hit;
     private void Awake() {
         rb = GetComponent<Rigidbody>();
+        playerControls = new PlayerInputActions();
     }
     private void OnEnable() {
-        playerControls.Enable();
+        move = playerControls.Player.Move;
+        fire = playerControls.Player.Fire;
+        interact = playerControls.Player.Interact;
+        fire.performed += Fire;
+        interact.performed += Interact;
+        interact.Enable();
+        fire.Enable();
+        move.Enable();
     }
     private void OnDisable() {
-        playerControls.Disable();
+        move.Disable();
+        fire.Disable();
+        interact.Disable();
     }
     private void Update() {
-        move = playerControls.ReadValue<Vector3>();
+        moveDirection = move.ReadValue<Vector3>();
+        PlayerInteract();
     }
     private void FixedUpdate() {
-            move = move.x * transform.right + move.z * transform.forward;
-         rb.MovePosition(transform.position + move * speed * Time.deltaTime);
+        moveDirection = moveDirection.x * transform.right + moveDirection.z * transform.forward;
+        rb.MovePosition(transform.position + moveDirection * speed * Time.deltaTime);
+    }
+
+    private void Fire(InputAction.CallbackContext context){
+        print("Fire");
+    }
+
+    private void Interact(InputAction.CallbackContext context){
+        print("Interact");
+    }
+
+    private void PlayerInteract(){
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        if(Physics.Raycast(transform.position,transform.forward, out hit, 2f,LayerMask.GetMask("Interactable"))){
+            InteractableObject detectedInteractableObject = hit.collider.GetComponent<InteractableObject>();
+            if(detectedInteractableObject != null){
+                currentInteractableObject = detectedInteractableObject;
+                currentInteractableObject.EnableOutline();
+            }
+        }
+        else{
+            if(currentInteractableObject != null){
+                currentInteractableObject.DisableOutline();
+                currentInteractableObject = null;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.F) && currentInteractableObject != null){
+            // UIManager.instance.UIInteractText.Show();
+            currentInteractableObject.Interacted();
+        }
     }
 }
